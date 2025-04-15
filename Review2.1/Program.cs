13a8bf8f299947afe86cf228e28c377c481b8ab6
denzelsilvestre <denzelsilvestre@DENZELSILVE132D>
2025-04-14 19:16:46 -0500

    Add project files.

---
+++ Review2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Interest Calculation Program");
+
+
+double Principle, Rate, Time, Interest;
+
+
+Console.WriteLine("What is princippal amount of the loan in dollars?");//principle 
+Principle = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What is interest rate?");//rate
+ Rate = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Total interest of loan in years?");//time
+Time = Convert.ToInt32(Console.ReadLine());
+
+Interest = Principle * Rate * Time;//equation
+
+Console.WriteLine("Total interest of loan:" + Interest);
