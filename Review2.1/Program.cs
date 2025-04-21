f126dcab7cd43de6b6601bfaf0e815ba03e16bf8
dsilvestreperez <dsilvestreperez@amarillocollege.com>
2025-04-20 19:38:18 -0500

    Update Program.cs

--- Review2.1/Program.cs
+++ Review2.1/Program.cs
@@ -1,20 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Interest Calculation Program");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoledocumation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interest Calculation Program");
 
 
-double Principle, Rate, Time, Interest;
+            double Principle, Rate, Time, Interest;//input the equation
 
 
-Console.WriteLine("What is princippal amount of the loan in dollars?");//principle 
-Principle = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is princippal amount of the loan in dollars?");//principle 
+            Principle = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("What is interest rate?");//rate
- Rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is interest rate?");//rate
+            Rate = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine("Total interest of loan in years?");//time
-Time = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total interest of loan in years?");//time
+            Time = Convert.ToInt32(Console.ReadLine());
 
-Interest = Principle * Rate * Time;//equation
+            Interest = Principle * Rate * Time;//equation
 
-Console.WriteLine("Total interest of loan:" + Interest);
+            Console.WriteLine("Total interest of loan:" + Interest);// intrest
+
+        }
+
+    }
+
+}
